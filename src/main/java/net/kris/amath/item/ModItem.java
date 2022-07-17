0dcafa7bd60eca6c92df5c1f361b6ff7ca4c1dbb
KrisanaponMeksamoot <kriskrisanapon@gmail.com>
2022-07-17 09:47:56 +0700

    Update for github

--- src/main/java/net/kris/amath/item/ModItem.java
+++ src/main/java/net/kris/amath/item/ModItem.java
@@ -7,17 +7,17 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItem {
-    public static final Item GAME_BOARD = registerItem("game_board",
-            new BoardBuilderItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
-    public static final Item GAME_BOARD_REMOVER = registerItem("game_board_remover",
-            new BoardRemoverItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
-    public static final Item TILE_BUNDLE = registerItem("tile_bundle",
-            new TilesBundleItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
+        public static final Item GAME_BOARD = registerItem("game_board",
+                        new BoardBuilderItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
+        public static final Item GAME_BOARD_REMOVER = registerItem("game_board_remover",
+                        new BoardRemoverItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
+        public static final Item TILE_BUNDLE = registerItem("tile_bundle",
+                        new TilesBundleItem(new FabricItemSettings().group(ModItemGroup.A_MATH)));
 
-    private static Item registerItem(String name, Item item) {
-        return Registry.register(Registry.ITEM, new Identifier(AMath.MODID, name), item);
-    }
+        private static Item registerItem(String name, Item item) {
+                return Registry.register(Registry.ITEM, new Identifier(AMath.MODID, name), item);
+        }
 
-    public static void registerModItems() {
-    }
+        public static void registerModItems() {
+        }
 }
